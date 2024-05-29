ba5b4604c17d1c96217478c54d1c35be39eb891f
vicentefredes <vice.fredes@duocuc.cl>
2024-05-28 22:58:48 -0400

    commit - alumnos en tabla

--- alumnos/views.py
+++ alumnos/views.py
@@ -4,6 +4,6 @@ from . models import Alumno, Genero
 # Create your views here.
 
 def index(request):
-    alumnos = Alumno.objects.all()
+    alumnos = Alumno.objects.all().order_by('apellido_paterno')
     context={'alumnos':alumnos}
     return render(request, 'alumnos/index.html', context)
