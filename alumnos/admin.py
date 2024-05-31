6b2d14fcb00850cdf82158fd76d6949a4d06246b
vicentefredes <vice.fredes@duocuc.cl>
2024-05-31 13:21:58 -0400

    commit 31/05 - nuevos modelos

--- alumnos/admin.py
+++ alumnos/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Genero, Alumno
+from .models import Genero, Alumno, Ramo, Seccion, AlumnoSeccion
 
 # Register your models here.
 admin.site.register(Genero)
 admin.site.register(Alumno)
+admin.site.register(Ramo)
+admin.site.register(Seccion)
+admin.site.register(AlumnoSeccion)
--- alumnos/models.py
+++ alumnos/models.py
@@ -23,4 +23,29 @@ class Alumno(models.Model):
     activo           = models.IntegerField()
 
     def __str__(self):
-        return str(self.nombre)+" "+str(self.apellido_paterno)   
+        return str(self.nombre)+" "+str(self.apellido_paterno)+" "+str(self.apellido_materno) 
+
+class Ramo(models.Model):
+    id_ramo  = models.AutoField(db_column='idRamo', primary_key=True) 
+    ramo     = models.CharField(max_length=20, blank=False, null=False)
+    creditos = models.IntegerField()
+    escuela  = models.CharField(max_length=45)
+
+    def __str__(self):
+        return str(self.ramo)
+
+class Seccion(models.Model):
+    id_seccion         = models.AutoField(db_column='idSeccion', primary_key=True) 
+    codigo_seccion     = models.CharField(max_length=20, blank=False, null=False)
+    id_ramo            = models.ForeignKey('Ramo',on_delete=models.CASCADE, db_column='idRamo') 
+
+    def __str__(self):
+        return str(self.codigo_seccion)
+
+class AlumnoSeccion(models.Model):
+    id_alumnoseccion  = models.AutoField(db_column='idAlumnoSeccion', primary_key=True) 
+    id_seccion        = models.ForeignKey('Seccion',on_delete=models.CASCADE, db_column='idSeccion') 
+    id_alumno         = models.ForeignKey('Alumno',on_delete=models.CASCADE, db_column='rut') 
+
+    def __str__(self):
+        return str(self.id_alumno)+" - "+str(self.id_seccion)
