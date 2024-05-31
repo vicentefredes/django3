0e952a6481e1b19a17a260d17b585216318ef8b7
vicentefredes <vice.fredes@duocuc.cl>
2024-05-31 14:00:14 -0400

    commit 31/05 3 - listadoSQL

--- alumnos/views.py
+++ alumnos/views.py
@@ -7,3 +7,8 @@ def index(request):
     alumnos = Alumno.objects.all().order_by('apellido_paterno')
     context={'alumnos':alumnos}
     return render(request, 'alumnos/index.html', context)
+
+def listadoSQL(request):
+    alumnos = Alumno.objects.raw('SELECT * FROM alumnos_alumno ORDER BY apellido_paterno')
+    context={'alumnos':alumnos}
+    return render(request, 'alumnos/listadoSQL.html', context)
