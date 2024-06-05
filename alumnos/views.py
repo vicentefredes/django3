ace992b9246f61cbf336320d378f6efff64572d7
vicentefredes <vice.fredes@duocuc.cl>
2024-06-04 23:47:28 -0400

    commit 06/04 - mostrar mensaje

--- alumnos/views.py
+++ alumnos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Alumno, Genero
 
 # Create your views here.
@@ -14,70 +14,50 @@ def listadoSQL(request):
     return render(request, 'alumnos/listadoSQL.html', context)
 
 def crud(request):
-    alumnos = Alumno.objects.all()
+    alumnos = Alumno.objects.all().order_by('apellido_paterno')
     context = {'alumnos':alumnos}
     return render(request, 'alumnos/alumnos_list.html', context)
 
 def alumnosAdd(request):
     if request.method != "POST":
-
         generos = Genero.objects.all()
-
         context = {"generos": generos}
-
         return render(request, 'alumnos/alumnos_add.html', context)
     else:
+        rut = request.POST["rut"]
+        nombre = request.POST["nombre"]
+        apaterno = request.POST["apaterno"]
+        amaterno = request.POST["amaterno"]
+        fechaNac = request.POST["fechaNac"]
+        genero = request.POST["genero"]
+        telefono = request.POST["telefono"]
+        email = request.POST["email"]
+        direccion = request.POST["direccion"]
+        activo = "1"
+        
+        objGenero = Genero.objects.get(id_genero=genero)
 
-            rut = request.POST["rut"]
-
-            nombre = request.POST["nombre"]
-
-            apaterno = request.POST["apaterno"]
-
-            amaterno = request.POST["amaterno"]
-
-            fechaNac = request.POST["fechaNac"]
-
-            genero = request.POST["genero"]
-
-            telefono = request.POST["telefono"]
-
-            email = request.POST["email"]
-
-            direccion = request.POST["direccion"]
-
-            activo = "1"
-
-
-
-            objGenero = Genero.objects.get(id_genero=genero)
-
-            obj = Alumno.objects.create(
-
+        obj = Alumno.objects.create(
             rut=rut,
-
             nombre=nombre,
-
             apellido_paterno=apaterno,
-
             apellido_materno=amaterno,
-
             fecha_nacimiento=fechaNac,
-
             id_genero=objGenero, # Asociar el genero correctamente
-
             telefono=telefono,
-
             email=email,
-
             direccion=direccion,
-
             activo=activo
-
             )
 
-            obj.save()
+        obj.save()
 
-            context = {'mensaje': "OK, datos grabados..."}
+        if objGenero.genero == 'Masculino':
+            mensaje = f"{nombre} {apaterno} {amaterno} ha sido agregado exitosamente"
+        else:
+            mensaje = f"{nombre} {apaterno} {amaterno} ha sido agregada exitosamente"
 
-            return render(request, 'alumnos/alumnos_add.html', context)
+        context = {'mensaje': mensaje}
+
+        #return redirect('crud')
+        return render(request, 'alumnos/alumnos_add.html', context)
