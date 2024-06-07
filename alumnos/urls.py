9712add6354ddf5afac3c56195e9d1e83b2383ab
vicentefredes <vice.fredes@duocuc.cl>
2024-06-06 20:50:21 -0400

    commit 06/06 - Update y Delete del PPT

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('index', views.index, name='index'),
     path('listadoSQL', views.listadoSQL, name='listadoSQL'),
     path('crud', views.crud, name='crud'),
-    path('alumnosAdd', views.alumnosAdd, name='alumnosAdd')
+    path('alumnosAdd', views.alumnosAdd, name='alumnosAdd'),
+    path('alumnos_findEdit/<str:pk>', views.alumnos_findEdit, name='alumnos_findEdit'),
+    path('alumnosUpdate', views.alumnosUpdate, name='alumnosUpdate'),
+    path('alumnos_del/<str:pk>', views.alumnos_del, name='alumnos_del'),
 ]
--- alumnos/views.py
+++ alumnos/views.py
@@ -61,3 +61,87 @@ def alumnosAdd(request):
 
         #return redirect('crud')
         return render(request, 'alumnos/alumnos_add.html', context)
+    
+def alumnos_findEdit(request, pk):
+
+    if pk != "":
+        alumno = Alumno.objects.get(rut=pk)
+        generos = Genero.objects.all()
+
+        print(type(alumno.id_genero.genero))
+
+        context = {'alumno':alumno, 'generos':generos}
+
+        if alumno:
+            return render(request, 'alumnos/alumnos_edit.html', context)
+        else:
+            mensaje = f"ERROR: el rut {pk} no existe"
+            context = {'mensaje': mensaje}
+            return render(request, 'alumnos/alumnos/alumnos_list.html', context)
+
+
+def alumnosUpdate(request):
+    if request.method != "POST":
+        alumnos = Alumno.objects.all()
+        context = {'alumnos': alumnos}
+        return render(request, 'alumnos/alumnos_list.html', context)
+    else:
+        rut = request.POST["rut"]
+        nombre = request.POST["nombre"]
+        apaterno = request.POST["apaterno"]
+        amaterno = request.POST["amaterno"]
+        fechaNac = request.POST["fechaNac"]
+        genero = request.POST["genero"]
+        telefono = request.POST["telefono"]
+        email = request.POST["email"]
+        direccion = request.POST["direccion"]
+        activo = "1"
+        
+        objGenero = Genero.objects.get(id_genero = genero)
+
+        alumno = Alumno()
+
+        alumno.rut=rut
+        alumno.nombre=nombre
+        alumno.apellido_paterno=apaterno
+        alumno.apellido_materno=amaterno
+        alumno.fecha_nacimiento=fechaNac
+        alumno.id_genero=objGenero
+        alumno.telefono=telefono
+        alumno.email=email
+        alumno.direccion=direccion
+        alumno.activo=activo
+
+        alumno.save()
+
+        if objGenero.genero == 'Masculino':
+            mensaje = f"{nombre} {apaterno} {amaterno} ha sido agregado exitosamente"
+        else:
+            mensaje = f"{nombre} {apaterno} {amaterno} ha sido agregada exitosamente"
+
+        generos = Genero.objects.all()
+
+        context = {'mensaje': mensaje, 'generos':generos, 'alumno':alumno}
+
+        #return redirect('crud')
+        return render(request, 'alumnos/alumnos_add.html', context)
+
+
+
+def alumnos_del(request, pk):
+    context = {}
+
+    try:
+        alumno = Alumno.objects.get(rut=pk)
+
+        alumno.delete()
+        mensaje = f"{alumno.nombre} {alumno.apellido_paterno} {alumno.apellido_materno} ha sido eliminado"
+        alumnos = Alumno.objects.all()
+        context = {'alumnos': alumnos, 'mensaje': mensaje}
+        return render(request, 'alumnos/alumnos/alumnos_list.html', context)
+    
+    except:
+        mensaje = f"ERROR: el rut {pk} no existe"
+        alumnos = Alumno.objects.all()
+        context = {'alumnos': alumnos, 'mensaje': mensaje}
+        return render(request, 'alumnos/alumnos/alumnos_list.html', context)
